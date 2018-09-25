171c41b9fb830109a83eb8d99ad0366a274e5957
KIRUTHIGA <KIRUTHIGA@10.129.115.54>
2018-09-25 10:34:17 +0530

    Second Commit

---
+++ src/main/java/com/example/secondapi/SecondApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.secondapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondApiApplication.class, args);
+	}
+}
